5a705acf536f0911f4eabe847eee719055d76769
kirti0609 <111431639+kirti34432@users.noreply.github.com>
2022-09-12 12:39:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-
 streamlit.title('My parents are amazing')
-
-streamlit.title('i love them')
+streamlit.title('breakfast Menu')
+streamlit.title('Omega 3 & BlueBerry Oatmeal')
+streamlit.title('Kale,Spinach & Rocket Smoothie')
+streamlit.title('Hard Boiled Free Range Egg')
